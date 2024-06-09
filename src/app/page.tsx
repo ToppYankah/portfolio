659057fb51255575abc79c12397f98854e9691ac
Kenny Yankz <tppyankah@gmail.com>
2024-06-09 08:09:47 +0000

    Setup fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
-      Hello Porfolio
+      <h1 className="font-serif text-4xl font-bold">
+        Welcome to Kenny's Portfolio
+      </h1>
     </main>
   );
 }
