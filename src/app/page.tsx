6c1cbda0ecab2a42b125a9fa28b68affb7d2d12b
Kenny Yankz <tppyankah@gmail.com>
2024-06-09 08:36:51 +0000

    Push to redeploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
-      <h1 className="font-serif text-4xl font-bold">
-        Welcome to Kenny's Portfolio
-      </h1>
+      <h1 className="font-serif text-4xl font-bold">Welcome to My Portfolio</h1>
     </main>
   );
 }
